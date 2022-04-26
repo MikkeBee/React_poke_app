c4e13a40476f20cc83a014bc58fd387d77ef0099
MikkeBee <mjblakley87@gmail.com>
2022-04-26 18:24:25 +0300

    add mapping to pokecard

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="Home">Home coming</div>;
+  return <div className="Home">It's a homepage, innit?</div>;
 };
 
 export default Home;
--- src/components/PokeSingle.js
+++ src/components/PokeSingle.js
@@ -15,13 +15,25 @@ class PokeSingle extends Component {
   render() {
     return (
       <div>
-        <h3>{this.props.params.pokesingle}</h3>Single Pokemon will be here.
+        <h3>{this.props.params.pokesingle}</h3>
         {/* use fetch to get the data, must use class component to get it
           Otherwise must use hooks. If using hooks, cannot use params. */}
         <img
           src={this.state.data.sprites?.other.dream_world.front_default}
           alt={this.state.data.name}
         />
+        {/* <p>{this.state.data.types.map((pokeType) => pokeType.type.name)}</p> */}
+        <div>
+          <p>Base XP: {this.state.data.base_experience}</p>
+          <p>Height: {this.state.data.height}</p>
+          <p>Weight: {this.state.data.weight}</p>
+          <p>
+            Type:{" "}
+            {this.state.data.types
+              ?.map((pokeType) => pokeType.type.name)
+              .join(", ")}
+          </p>
+        </div>
       </div>
     );
   }
