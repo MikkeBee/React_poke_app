8dd240a6fa536693c2adba427cbaf3a39f6f0acd
MikkeBee <mjblakley87@gmail.com>
2022-04-27 12:22:40 +0300

    add home link to poke image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 import pokelogo from "../assets/images/pokelogo.png";
 
 const Header = () => {
   return (
     <header className="Header">
       <div>
-        <img src={pokelogo} />
+        <Link to="/">
+          <img src={pokelogo} />
+        </Link>
       </div>
       <Nav />
     </header>
